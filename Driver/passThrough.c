72d39001e243226049f47e0f9e80a0d5399d5ee4
Hachico1337 <97884641+Hachico1337@users.noreply.github.com>
2024-05-21 00:35:48 +0300

    check by proc name

--- Driver/passThrough.c
+++ Driver/passThrough.c
@@ -24,10 +24,9 @@ Environment:
 #include <suppress.h>
 const  uint8_t* KEY[32] = { 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC };
 const  uint8_t* IV[16] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
-#define LAB_IN L"bagdasaryan"
 #pragma prefast(disable:__WARNING_ENCODE_MEMBER_FUNCTION_POINTER, "Not valid for kernel mode drivers")
 
-
+#define USERMODE_NAME "usermode_ex.ex"
 PFLT_FILTER gFilterHandle;
 ULONG_PTR OperationStatusCtx = 1;
 
@@ -549,7 +548,7 @@ Return Value:
 --*/
 {
    
-    DbgPrintEx(0, 0, "Hello mne");
+    DbgPrintEx(0, 0, "Hello mne\n");
     NTSTATUS status;
 
     UNREFERENCED_PARAMETER( RegistryPath );
@@ -620,7 +619,38 @@ Return Value:
 }
 
 
+NTSTATUS FindProcessByName(CHAR* process_name, PEPROCESS* process)
+{
+    //https://www.vergiliusproject.com/kernels/x64/Windows%2011/22H2%20(2022%20Update)/_EPROCESS
+    PEPROCESS sys_process = PsInitialSystemProcess;
+    PEPROCESS cur_entry = sys_process;
 
+    CHAR image_name[15];
+
+    do
+    {
+        RtlCopyMemory((PVOID)(&image_name), (PVOID)((uintptr_t)cur_entry + 0x5a8) /*EPROCESS->ImageFileName*/, sizeof(image_name));
+
+        if (strstr(image_name, process_name))
+        {
+
+            ULONG  active_threads;
+
+            RtlCopyMemory((PVOID)&active_threads, (PVOID)((uintptr_t)cur_entry + 0x5f0) /*EPROCESS->ActiveThreads*/, sizeof(active_threads));
+            if (active_threads)
+            {
+                *process = cur_entry;
+                return STATUS_SUCCESS;
+            }
+        }
+
+        PLIST_ENTRY list = (PLIST_ENTRY)((uintptr_t)(cur_entry) + 0x448) /*EPROCESS->ActiveProcessLinks*/;
+        cur_entry = (PEPROCESS)((uintptr_t)list->Flink - 0x448);
+
+    } while (cur_entry != sys_process);
+
+    return STATUS_NOT_FOUND;
+}
 FLT_PREOP_CALLBACK_STATUS
 PtPreOperationPassThrough(
     _Inout_ PFLT_CALLBACK_DATA Data,
@@ -639,36 +669,29 @@ PtPreOperationPassThrough(
 
     NTSTATUS status;
 
-    PFLT_FILE_NAME_INFORMATION NameInfo = NULL;
-    status = FltGetFileNameInformation(
-        Data,
-        FLT_FILE_NAME_NORMALIZED |
-        FLT_FILE_NAME_QUERY_DEFAULT,
-        &NameInfo);
-    UNICODE_STRING required_extension = RTL_CONSTANT_STRING(LAB_IN);
-    if (NT_SUCCESS(status))
-    {
-        if (RtlEqualUnicodeString(&required_extension, &NameInfo->Extension, FALSE) == TRUE) {
 
-            if (Data->Iopb->MajorFunction == IRP_MJ_WRITE) {
-        
-                if (Data->Iopb->Parameters.Write.WriteBuffer && Data->Iopb->Parameters.Write.Length) {
-  
-                   struct AES_ctx ctx;
-                   AES_init_ctx_iv(&ctx, KEY, IV);
-                   AES_CTR_xcrypt_buffer(&ctx, Data->Iopb->Parameters.Write.WriteBuffer, Data->Iopb->Parameters.Write.Length);
-                   //
-                   //xorvalue = value ^ xorkey
-                   //valaue == xorvalue ^ xorkey
-                   //
-               
 
-                }
+
+    if (Data->Iopb->MajorFunction == IRP_MJ_WRITE) {
+
+        if (Data->Iopb->Parameters.Write.WriteBuffer && Data->Iopb->Parameters.Write.Length) {
+            PEPROCESS client_proc = 0;
+            FindProcessByName(USERMODE_NAME, &client_proc);
+
+
+            auto proc = FltGetRequestorProcess(Data);
+            if (client_proc && proc && (DWORD32)client_proc == proc)
+            {
+                struct AES_ctx ctx;
+                AES_init_ctx_iv(&ctx, KEY, IV);
+                AES_CTR_xcrypt_buffer(&ctx, Data->Iopb->Parameters.Write.WriteBuffer, Data->Iopb->Parameters.Write.Length);
             }
+
+
         }
+
+
     }
-
-
 
 
     if (PtDoRequestOperationStatus(Data)) {
@@ -753,37 +776,26 @@ PtPostOperationPassThrough(
 )
 {
 
-    NTSTATUS status;
+    if (Data->Iopb->MajorFunction == IRP_MJ_READ) {
 
-    PFLT_FILE_NAME_INFORMATION NameInfo = NULL;
-    status = FltGetFileNameInformation(
-        Data,
-        FLT_FILE_NAME_NORMALIZED |
-        FLT_FILE_NAME_QUERY_DEFAULT,
-        &NameInfo);
-    UNICODE_STRING required_extension = RTL_CONSTANT_STRING(LAB_IN);
-    if (NT_SUCCESS(status))
-    {
-        if (RtlEqualUnicodeString(&required_extension, &NameInfo->Extension, FALSE) == TRUE) {
+      if (Data->Iopb->Parameters.Read.ReadBuffer && Data->Iopb->Parameters.Read.Length) {
 
-            if (Data->Iopb->MajorFunction == IRP_MJ_READ) {
+            PEPROCESS client_proc = 0;
+            FindProcessByName(USERMODE_NAME, &client_proc);
 
-                if (Data->Iopb->Parameters.Read.ReadBuffer && Data->Iopb->Parameters.Read.Length) {
-   
-    
-                    struct AES_ctx ctx;
-                    AES_init_ctx_iv(&ctx, KEY, IV);
-                    AES_CTR_xcrypt_buffer(&ctx, Data->Iopb->Parameters.Read.ReadBuffer, Data->Iopb->Parameters.Read.Length);
-              //
-              //xorvalue = value ^ xorkey
-              //value == xorvalue ^ xorkey
-              //
-                }
-     
+
+            auto proc = FltGetRequestorProcess(Data);
+            if (client_proc && proc && (DWORD32)client_proc == proc)
+            {
+              struct AES_ctx ctx;
+              AES_init_ctx_iv(&ctx, KEY, IV);
+              AES_CTR_xcrypt_buffer(&ctx, Data->Iopb->Parameters.Read.ReadBuffer, Data->Iopb->Parameters.Read.Length);
             }
-        }
-    }
+                  
+      }
 
+    }
+   
     return FLT_POSTOP_FINISHED_PROCESSING;
 }
 
